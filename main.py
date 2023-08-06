13995cb4d1f40cfa446f3c710f31769f850b5aa3
Lizzie Siegle <esiegle@brynmawr.edu>
2023-08-05 18:34:27 -0700

    no search

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 # from langchain.chat_models import ChatOpenAI
-from googleapiclient.discovery import build
+from apiclient.discovery import build
 import http.client
-from langchain import LLMMathChain, OpenAI, SerpAPIWrapper
-from langchain.agents import initialize_agent, Tool
+from langchain import LLMMathChain, OpenAI
+from langchain.agents import initialize_agent, load_tools
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
@@ -24,17 +24,8 @@ st.subheader('enter details below')
 
 config = dotenv_values(".env")
 os.environ["OPENAI_API_KEY"] = config.get('OPENAI_API_KEY')
-os.environ["SERPAPI_API_KEY"] = config.get('SERPAPI_API_KEY')
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
-search = SerpAPIWrapper()
 llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
-tools = [
-    Tool(
-        name = "Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions"
-    )
-]
 #validate email
 email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 def validate_email(email):
@@ -43,7 +34,7 @@ def validate_email(email):
         return True
     else:
         return False
-    
+tools = load_tools(["llm-math"], llm=llm)
 agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
 with st.form('my_form'):
     email = st.text_input('Email to send concert plan to')
