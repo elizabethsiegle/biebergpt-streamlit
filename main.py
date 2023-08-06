a7fcccb0bc6bc89c888ddc32053daed3b670589f
Boyd B. Lever <boydlever@stanfordhacker.com>
2023-08-06 14:56:05 -0700

    geoCityId=jambase:4

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ with st.form('my_form'):
             print(f"artistByMood {artistByMood}")
             conn = http.client.HTTPSConnection("www.jambase.com") 
             jambaseapikey = st.secrets["JAMBASE_API_KEY"]
-            conn.request("GET", f"/jb-api/v1/events?eventType=concerts&geoCityId=jambase%3A4&artistName={artistByMood}&apikey={jambaseapikey}", headers=headers)
+            conn.request("GET", f"/jb-api/v1/events?eventType=concerts&geoCityId=jambase:4&artistName={artistByMood}&apikey={jambaseapikey}", headers=headers)
             jamurl = f"/jb-api/v1/events?eventType=concerts&artistName={artistByMood}&apikey={jambaseapikey}"
             res = conn.getresponse()
             data = res.read()
