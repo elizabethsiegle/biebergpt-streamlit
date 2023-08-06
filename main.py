efaf698ebf44516f128cc2fadd13e1fd6dc349b1
Lizzie Siegle <esiegle@brynmawr.edu>
2023-08-05 20:56:38 -0700

    OSL to outside lands

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import json
 st.title('BieberGPT🤖🎶')
 st.image('https://s.france24.com/media/display/ba80de5a-c06a-11eb-9594-005056bf87d6/w:1280/p:1x1/000_1OG6IB.jpg',width=100)
 st.subheader('Enter details below to get an AI-generated Bieber song and song from a select Outside Lands performer🎤 according to your mood! You will also get emailed📧 a concert of an OSL performer from the Jambase API')
-st.write('Miss out on OSL tickets? Catch some Outside Lands performers at other concerts! Data received from the JamBase API')
+st.write('Miss out on Outside Lands tickets? Catch some Outside Lands performers at other concerts! Data received from the JamBase API')
 st.image('https://storage.tally.so/d9947039-48cb-4392-8636-1cacdf028a21/Frame-2-4-.png',width=100 )
 st.write("Made with ❤️ at Outside🎶 LLMs⛓️ 2023")
 st.write("[Code on GitHub](https://github.com/elizabethsiegle/biebergpt-streamlit)")
